8c3879345cc990c512e071f73ca8aca8731ca5ec
anil.parihar <rana.anil516@gmail.com>
2026-08-07 18:37:06 +0530

    feat(sidebar): render dynamic navigation menu using @for

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { SidebarMenu } from '../../shared/models/sidebar-menu';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
-  sideMenus = [ 'Employees', 'Deptarments', 'Projects', 'Reports', 'Payroll', 'Settings'];
-  menuObj = [
-    {name: 'Dashboards', icon:"bi bi-motherboard"},
-    {name: 'Depts', icon:"bi bi-house"},
-    {name: 'Projects', icon:"bi bi-kanban"},
-    {name: 'Reports', icon:"bi bi-clipboard-data"},
-    {name: 'Payroll', icon:"bi bi-wallet2"},
-    {name: 'Settings', icon:"bi bi-gear"}
-  ]
+  selectedMenuId = 1;
+  sideBarMenus :SidebarMenu[] = [
+    {id: 1, name: 'Dashboards', icon:"bi bi-motherboard", route:'/dashboard'},
+    {id: 2, name: 'Depts', icon:"bi bi-house", route:'/dept'},
+    {id: 3, name: 'Projects', icon:"bi bi-kanban", route:'/project'},
+    {id: 4, name: 'Reports', icon:"bi bi-clipboard-data", route:'/report'},
+    {id: 5, name: 'Payroll', icon:"bi bi-wallet2", route:'/payroll'},
+    {id: 6, name: 'Settings', icon:"bi bi-gear", route:'/setting'}
+  ];
+
+
+  selectedMenuItem(item: SidebarMenu){
+    console.log(item);
+    this.selectedMenuId = item.id;
+    
+  }
 }
